e8ff234e451f4f81bd2e46de9559ccf5b73ae9c3
lisaazhaang <lisazhang@oakland.edu>
2021-11-14 18:33:59 -0500

    updated agent class

--- lib/agent.py
+++ lib/agent.py
@@ -23,5 +23,30 @@ class Agent():
 
     @classmethod
     def from_license_number(cls, license_number: int, db_connection: mysql.connector.MySQLConnection):
-        """Returns an agent from the database based on their license number"""
+        #Returns an agent from the database based on their license number
+         with db_connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM AGENT WHERE license_number = %s", (license_number))
+            agent_data = cursor.fetchone()
+            return cls(**agent_data)
+        
+        pass
+
+    @classmethod
+    def create_agent(cls, db_connection: mysql.connector.MySQLConnection,  license_number: int, first_name: str, middle_initial: str, last_name: str, office_area_code: int, office_phone_number: int, cell_area_code: int, cell_phone_number: int, email_address: str, office_street_address: str, office_city: str, office_state: str, office_zip_code: int, company_id: int, client_ids: list, listing_ids: list):
+        #Creates a listing in the database
+        with db_connection.cursor() as cursor:
+            cursor.execute("INSERT INTO AGENT " 
+            "(`agent_license_number`, `agent_first_name`, `agent_middle_initial`,`agent_last_name`, `agent_office_area_code`, `agent_office_phone_number`, `agent_mobile_area_code`,`agent_mobile_phone_number`, `agent_email_address`, `agent_office_street`, `agent_office_city`,`agent_office_state`, `agent_office_zip`, `agent_company_id`, `CLIENT_id`, `LISTING_mls_number`) "
+            "values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,)", (license_number, first_name, middle_initial,last_name, office_area_code, office_phone_number, cell_area_code, cell_phone_number, email_address, office_street_address, office_city, office_state, office_zip_code, company_id:, client_ids, listing_ids))
+
+        pass
+
+    @classmethod
+    def delete_agent(cls, license_number: int, db_connection: mysql.connector.MySQLConnection):
+        #Deletes agent
+         with db_connection.cursor() as cursor:
+            cursor.execute("DELETE FROM AGENT WHERE license_number = %s", (license_number))
+            agent_data = cursor.fetchone()
+            return cls(**agent_data)
+        
         pass
