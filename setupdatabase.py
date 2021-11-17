3c56e7afb83bac93c173d175e567bfbdab574c6a
Robert Drescher <robert@macombhometeam.com>
2021-11-17 15:43:48 -0500

    Changes to setup script

---
+++ setupdatabase.py
@@ -0,0 +1,14 @@
+import mysql.connector as con
+import json
+
+with open("./sql/create_schema.sql", "r") as fp: setup_sql = fp.read()
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+db_connection = con.connect(**config)
+
+with db_connection.cursor() as cursor:
+    cursor.execute(setup_sql, multi=True)
+    db_connection.commit()
+
+db_connection.close()
--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema dbproject
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema dbproject
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+-- CREATE SCHEMA IF NOT EXISTS `dbproject` DEFAULT CHARACTER SET utf8 ;
+-- USE `dbproject` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`LISTING`
+-- Table `dbproject`.`LISTING`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`LISTING` (
+CREATE TABLE IF NOT EXISTS `dbproject`.`LISTING` (
   `listing_mls_number` INT NOT NULL AUTO_INCREMENT,
   `listing_type` VARCHAR(45) NOT NULL,
   `listing_status` VARCHAR(45) NOT NULL,
@@ -33,11 +33,11 @@ CREATE TABLE IF NOT EXISTS `mydb`.`LISTING` (
   `listing_address_street` VARCHAR(45) NOT NULL,
   `listing_address_city` VARCHAR(45) NOT NULL,
   `listing_address_state` VARCHAR(45) NOT NULL,
-  `listing_address_zip` MEDIUMINT(5) NOT NULL,
+  `listing_address_zip` MEDIUMINT NOT NULL,
   `listing_structure_style` VARCHAR(45) NOT NULL,
-  `listing_bedroom_count` SMALLINT(3) NOT NULL,
-  `listing_full_bath_count` SMALLINT(3) NOT NULL,
-  `listing_half_bath_count` SMALLINT(3) NOT NULL,
+  `listing_bedroom_count` SMALLINT NOT NULL,
+  `listing_full_bath_count` SMALLINT NOT NULL,
+  `listing_half_bath_count` SMALLINT NOT NULL,
   `listing_basement_yn` TINYINT NOT NULL,
   `listing_waterfront_yn` TINYINT NOT NULL,
   `listing_fireplace_yn` TINYINT NOT NULL,
@@ -46,9 +46,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`LISTING` (
   `listing_ownership` VARCHAR(45) NOT NULL,
   `listing_school_district` VARCHAR(45) NOT NULL,
   `listing_garage_car_count` SMALLINT NOT NULL,
-  `listing_above_grade_sqft` MEDIUMINT(5) NOT NULL,
+  `listing_above_grade_sqft` MEDIUMINT NOT NULL,
   `listing_acreage` DECIMAL NOT NULL,
-  `listing_year_built` MEDIUMINT(4) NOT NULL,
+  `listing_year_built` MEDIUMINT NOT NULL,
   `listing_date` DATE NOT NULL,
   `listing_agent_license_number` INT NOT NULL,
   `listing_colisting_agent_license_number` INT NOT NULL,
@@ -58,7 +58,7 @@ ENGINE = InnoDB;
 
 /*
 insert into
-`mydb`.`LISTING`( `listing_mls_number`,`listing_type`,`listing_status`, `listing_description`,
+`dbproject`.`LISTING`( `listing_mls_number`,`listing_type`,`listing_status`, `listing_description`,
 `listing_sale_yn`,`listing_rent_yn`,`listing_price`,`listing_original_price`,`listing_address_number`,
 `listing_address_street`, `listing_address_city`, `listing_address_state`,
 `listing_address_zip`,`listing_structure_style`,`listing_bedroom_count`, `listing_full_bath_count`,
@@ -84,14 +84,14 @@ select * from LISTING;
 
 -- ----------
 -------------------------------------------
--- Table `mydb`.`CLIENT`
+-- Table `dbproject`.`CLIENT`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`CLIENT` (
+CREATE TABLE IF NOT EXISTS `dbproject`.`CLIENT` (
   `client_id` INT NOT NULL,
   `client_first_name` VARCHAR(45) NOT NULL,
   `client_middle_initial` VARCHAR(45) NULL,
   `client_last_name` VARCHAR(45) NOT NULL,
-  `client_area_code` MEDIUMINT(3) NOT NULL,
+  `client_area_code` MEDIUMINT NOT NULL,
   `client_phone_number` INT NOT NULL,
   `client_email_address` VARCHAR(45) NOT NULL,
   `client_agent_license_number` INT NOT NULL,
@@ -100,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`CLIENT` (
   INDEX `fk_CLIENT_LISTING1_idx` (`LISTING_mls_number` ASC) VISIBLE,
   CONSTRAINT `fk_CLIENT_LISTING1`
     FOREIGN KEY (`LISTING_mls_number`)
-    REFERENCES `mydb`.`LISTING` (`listing_mls_number`)
+    REFERENCES `dbproject`.`LISTING` (`listing_mls_number`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -108,7 +108,7 @@ ENGINE = InnoDB;
 
 /*
 insert into
-`mydb`.`CLIENT`(`client_id`, `client_first_name`, `client_middle_initial`, 
+`dbproject`.`CLIENT`(`client_id`, `client_first_name`, `client_middle_initial`, 
 `client_last_name`,`client_area_code`, `client_phone_number`, `client_email_address`,
 `client_agent_license_number`) values
 
@@ -122,22 +122,22 @@ select * from CLIENT;
 DELETE FROM CLIENT WHERE client_id='002';*/
 
 -- -----------------------------------------------------
--- Table `mydb`.`AGENT`
+-- Table `dbproject`.`AGENT`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`AGENT` (
+CREATE TABLE IF NOT EXISTS `dbproject`.`AGENT` (
   `agent_license_number` INT NOT NULL,
   `agent_first_name` VARCHAR(45) NOT NULL,
   `agent_middle_initial` VARCHAR(45) NULL,
   `agent_last_name` VARCHAR(45) NOT NULL,
-  `agent_office_area_code` MEDIUMINT(3) NOT NULL,
-  `agent_office_phone_number` INT(7) NOT NULL,
-  `agent_mobile_area_code` MEDIUMINT(3) NOT NULL,
-  `agent_mobile_phone_number` INT(7) NOT NULL,
+  `agent_office_area_code` MEDIUMINT NOT NULL,
+  `agent_office_phone_number` INT NOT NULL,
+  `agent_mobile_area_code` MEDIUMINT NOT NULL,
+  `agent_mobile_phone_number` INT NOT NULL,
   `agent_email_address` VARCHAR(45) NOT NULL,
   `agent_office_street` VARCHAR(45) NOT NULL,
   `agent_office_city` VARCHAR(45) NOT NULL,
   `agent_office_state` VARCHAR(45) NOT NULL,
-  `agent_office_zip` MEDIUMINT(5) NOT NULL,
+  `agent_office_zip` MEDIUMINT NOT NULL,
   `agent_company_id` INT NOT NULL,
   `CLIENT_id` INT NULL,
   `LISTING_mls_number` INT NULL,
@@ -146,19 +146,19 @@ CREATE TABLE IF NOT EXISTS `mydb`.`AGENT` (
   INDEX `fk_AGENT_LISTING1_idx` (`LISTING_mls_number` ASC) VISIBLE,
   CONSTRAINT `fk_AGENT_CLIENT1`
     FOREIGN KEY (`CLIENT_id`)
-    REFERENCES `mydb`.`CLIENT` (`client_id`)
+    REFERENCES `dbproject`.`CLIENT` (`client_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_AGENT_LISTING1`
     FOREIGN KEY (`LISTING_mls_number`)
-    REFERENCES `mydb`.`LISTING` (`listing_mls_number`)
+    REFERENCES `dbproject`.`LISTING` (`listing_mls_number`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 /*
 insert into 
-`mydb`.`AGENT`(`agent_license_number`, `agent_first_name`, `agent_middle_initial`,
+`dbproject`.`AGENT`(`agent_license_number`, `agent_first_name`, `agent_middle_initial`,
 `agent_last_name`, `agent_office_area_code`, `agent_office_phone_number`, `agent_mobile_area_code`,
 `agent_mobile_phone_number`, `agent_email_address`, `agent_office_street`, `agent_office_city`,
 `agent_office_state`, `agent_office_zip`, `agent_company_id`, `CLIENT_id`, `LISTING_mls_number`) values
@@ -171,17 +171,17 @@ DELETE FROM AGENT WHERE agent_license_number=999;*/
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`COMPANY`
+-- Table `dbproject`.`COMPANY`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`COMPANY` (
+CREATE TABLE IF NOT EXISTS `dbproject`.`COMPANY` (
   `company_id` INT NOT NULL,
   `company_hq_street` VARCHAR(45) NOT NULL,
   `company_hq_city` VARCHAR(45) NOT NULL,
   `company_hq_state` VARCHAR(45) NOT NULL,
-  `company_hq_zip` MEDIUMINT(5) NOT NULL,
-  `company_hq_phone_area` MEDIUMINT(3) NOT NULL,
+  `company_hq_zip` MEDIUMINT NOT NULL,
+  `company_hq_phone_area` MEDIUMINT NOT NULL,
   `company_hq_phone_number` INT NOT NULL,
-  `company_hq_fax_area` MEDIUMINT(3) NOT NULL,
+  `company_hq_fax_area` MEDIUMINT NOT NULL,
   `company_hq_fax_number` INT NOT NULL,
   `company_license_number` INT NOT NULL,
   `AGENT_license_number` INT NULL,
@@ -189,14 +189,14 @@ CREATE TABLE IF NOT EXISTS `mydb`.`COMPANY` (
   INDEX `fk_COMPANY_AGENT_idx` (`AGENT_license_number` ASC) VISIBLE,
   CONSTRAINT `fk_COMPANY_AGENT`
     FOREIGN KEY (`AGENT_license_number`)
-    REFERENCES `mydb`.`AGENT` (`agent_license_number`)
+    REFERENCES `dbproject`.`AGENT` (`agent_license_number`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 /*
 insert into
-`mydb`.`COMPANY`(`company_id`, `company_hq_street`, `company_hq_city`,`company_hq_state`,
+`dbproject`.`COMPANY`(`company_id`, `company_hq_street`, `company_hq_city`,`company_hq_state`,
 `company_hq_zip`, `company_hq_phone_area`, `company_hq_phone_number`,`company_hq_fax_area`,
 `company_hq_fax_number`,`company_license_number`,`AGENT_license_number`) values
 (123, 'Tree Street', 'Detroit', 'Michigan', 48309, 123, 4567891, 123, 1234123, 451, 001);
