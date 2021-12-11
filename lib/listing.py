7ed0cd299704aa841c39d7d16c118be6092ae89c
Robert Drescher <robert@macombhometeam.com>
2021-12-11 15:10:45 -0500

    Documentation

--- lib/listing.py
+++ lib/listing.py
@@ -64,12 +64,15 @@ class Listing:
             db_connection (mysql.connector.MySQLConnection): The database connection to use
 
         Returns:
-            [type]: [description]
+            Listing: The retrieved Listing or None if an invalid id was passed.
         """
         with db_connection.cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT * FROM LISTING WHERE listing_mls_number = %s", (id,))
-            listing_data = cursor.fetchone() # Haven't tested yet.
-            return cls(**listing_data)
+            try:
+                cursor.execute("SELECT * FROM LISTING WHERE listing_mls_number = %s", (id,))
+                listing_data = cursor.fetchone()
+                return cls(**listing_data)
+            except Exception:
+                return None
 
     @classmethod
     def create_listing(cls, db_connection: mysql.connector.MySQLConnection, listing_type: str, status: str, description: str, sale_yn: bool, rent_yn: bool, price: int, address_number: int, address_street: str, address_city: str, address_state: str, address_zip: int, structure_style: str, bedroom_count: int, full_bath_count: int, half_bath_count: int, basement_yn: bool, waterfront_yn: bool, fireplace_yn: bool, garage_yn: bool, pool_yn: bool, ownership: str, school_district: str, garage_car_count: int, above_grade_sqft: int, acreage: int, year_built: int, date_listed: date, agent_license_number: int, colisting_agent_license_number: int, image_link: str):
@@ -115,7 +118,7 @@ class Listing:
             cursor.execute("INSERT INTO LISTING (listing_type, listing_status, listing_description, listing_sale_yn, listing_rent_yn, listing_price, listing_original_price, listing_address_number, listing_address_street, listing_address_city, listing_address_state, listing_address_zip, listing_structure_style, listing_bedroom_count, listing_full_bath_count, listing_half_bath_count, listing_basement_yn, listing_waterfront_yn, listing_fireplace_yn, listing_garage_yn, listing_pool_yn, listing_ownership, listing_school_district, listing_garage_car_count, listing_above_grade_sqft, listing_acreage, listing_year_built, listing_date, listing_agent_license_number, listing_colisting_agent_license_number, listing_image_links) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (listing_type, status, description, sale_yn, rent_yn, price, price, address_number, address_street, address_city, address_state, address_zip, structure_style, bedroom_count, full_bath_count, half_bath_count, basement_yn, waterfront_yn, fireplace_yn, garage_yn, pool_yn, ownership, school_district, garage_car_count, above_grade_sqft, acreage, year_built, date_listed, agent_license_number, colisting_agent_license_number, image_link))
             mls_number = cursor.lastrowid
             db_connection.commit()
-            return cls.from_listing_id(mls_number, db_connection)
+        return cls.from_listing_id(mls_number, db_connection)
            
 
     def update_listing(self, db_connection: mysql.connector.MySQLConnection):
